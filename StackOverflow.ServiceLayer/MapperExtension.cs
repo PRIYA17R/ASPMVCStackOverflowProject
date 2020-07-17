c7e2c313befa1957600dc93813a1e25b8c005a65
shashank929 <rewaskar.priya@gmail.com>
2020-07-18 05:28:28 +0530

    created mapper and hash cryptograpgy class

---
+++ StackOverflow.ServiceLayer/MapperExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace StackOverflow.ServiceLayer
+{
+  public static  class MapperExtension
+    {
+        public static void IgnoreUnMappedProperties (TypeMap map, IMappingExpression expr)
+        {
+            foreach (string propName in map.GetUnmappedPropertyNames())
+            {
+                if(map.SourceType.GetProperty(propName) != null)
+                {
+                    expr.ForMember(propName, opt => opt.Ignore());
+                }
+                if (map.DestinationType.GetProperty(propName) != null)
+                {
+                    expr.ForMember(propName, opt => opt.Ignore());
+                }
+
+            }
+        }
+        public static void IgnoreUnMapped(this IProfileExpression profile)
+        {
+            profile.ForAllMaps(IgnoreUnMappedProperties);
+        }
+    }
+}
+
+
