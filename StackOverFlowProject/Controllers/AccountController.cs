91b85866be62a8e9da7f3ca4b2012b082138d710
shashank929 <rewaskar.priya@gmail.com>
2020-07-22 12:55:26 +0530

    added login and logout functionality

--- StackOverFlowProject/Controllers/AccountController.cs
+++ StackOverFlowProject/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http.ModelBinding;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 using stackOverflow.ViewModels;
 using StackOverflow.ServiceLayer;
 
@@ -25,7 +26,7 @@ namespace StackOverFlowProject.Controllers
         [HttpPost]
         public ActionResult Register(RegisterViewModel rvm)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 int uid = this.us.InsertUser(rvm);
                 Session["CurrentUserID"] = uid;
@@ -40,7 +41,59 @@ namespace StackOverFlowProject.Controllers
                 ModelState.AddModelError("x", "Invalid data");
                 return View();
             }
+
+        }
+        public ActionResult Login()
+
+        {
+            LoginViewModel lvm = new LoginViewModel();
+            return View(lvm);
+
+        }
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Login(LoginViewModel lvm)
+        {
+            if (ModelState.IsValid)
+            {
+               UserViewModel uvm = this.us.GetUsersByEmailAndPassword(lvm.Email, lvm.Password);
+                if(uvm!= null)
+                {
+                    Session["CurrentUserID"] = uvm.UserID;
+                    Session["CurrentUserName"] = uvm.Name;
+                    Session["CurrentEmail"] = uvm.Email;
+                    Session["CurrentUserPassword"] = uvm.Password;
+                    Session["CurrentUserIsAdmin"] = uvm.IsAdmin;
+                    if (uvm.IsAdmin)
+                    {
+                        return RedirectToRoute(new { area = "admin", Controller = "AdminHome", action = "Index" });
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+
+                else
+                {
+                    ModelState.AddModelError("x", "InValid Email or Password");
+                    return View(lvm);
+
+                }
+
+            }
+            else
+            {
+                ModelState.AddModelError("x", "InValid Data");
+                return View(lvm);
+            }
            
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
